265cb2298edc2eb6486e312765d3a7bf994e1afd
gergopistai <gergopistai@gmail.com>
2024-11-28 21:23:43 +0100

    Move MovieFlexbox style to index.css

--- frontend/src/components/MovieFlexbox.jsx
+++ frontend/src/components/MovieFlexbox.jsx
@@ -3,7 +3,7 @@ import { Movie } from "../components/Movie";
 const MovieFlexbox = ({ movieIdList }) => {
     if (movieIdList) {
         return (
-            <div className="movies-flexbox flex py-8 gap-4" >
+            <div className="movies-flexbox flex" >
                 {movieIdList.map((movieID) => (
                     <Movie key={movieID} movieID={movieID} />
                 ))}
