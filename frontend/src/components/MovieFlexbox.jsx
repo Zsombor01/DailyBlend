089f8319c8f170c83af892466e5902ea70eeadb8
Bíró Zsombor Zsolt <zs.zsombor.biro@gmail.com>
2024-11-28 15:54:53 +0100

    Adjusting the style of the profile page

--- frontend/src/components/MovieFlexbox.jsx
+++ frontend/src/components/MovieFlexbox.jsx
@@ -3,7 +3,7 @@ import { Movie } from "../components/Movie";
 const MovieFlexbox = ({ movieIdList }) => {
     if (movieIdList) {
         return (
-            <div className="movies-flexbox flex" >
+            <div className="movies-flexbox flex py-8 gap-4" >
                 {movieIdList.map((movieID) => (
                     <Movie key={movieID} movieID={movieID} />
                 ))}
